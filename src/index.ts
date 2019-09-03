b1cefe7ba065dc6ce5b34cb611b95a668cacb123
Karl Juri Ots <kjots@kjots.org>
2019-09-03 13:29:11 +1000

    Added create-typescript-monorepo app.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,73 @@
-export default function createTypescriptMonorepo() {
+import decompress from 'decompress';
+import handlebars from 'handlebars';
+import path from 'path';
+
+import { camelCase, upperFirst } from 'lodash';
+
+import { from, MonoTypeOperatorFunction, Observable } from 'rxjs';
+import { filter, map, mergeAll, tap } from 'rxjs/operators';
+
+import File = require('vinyl');
+
+import { dest } from '@kjots/vinyl-fs-observable';
+
+export interface Opts {
+  output: string;
+  name: string;
+  description: string;
+  keywords: Array<string>;
+}
+
+export const defaults: Partial<Opts> = {
+  output: '.',
+  name: path.basename(process.cwd()),
+  keywords: []
+};
+
+interface TemplateContext {
+  name: string;
+  nameCamelCase: string;
+  nameTitleCase: string;
+  description?: string;
+  keywords: Array<{ keyword: string, first: boolean, last: boolean }>;
+}
+
+const templateZip = path.resolve(__dirname, '../lib/typescript-monorepo-template.zip');
+
+export default function createTypescriptMonorepo(opts: Opts) {
+  const { name, description } = opts;
+  const nameCamelCase = camelCase(name);
+  const nameTitleCase = upperFirst(nameCamelCase);
+  const keywords = opts.keywords.map((keyword, i) => ({ keyword, first: i === 0, last: i === opts.keywords.length - 1 }));
+
+  readTemplateFiles()
+    .pipe(
+      applyTemplate({ name, nameCamelCase, nameTitleCase, description, keywords }),
+      dest(opts.output)
+    );
+}
+
+function readTemplateFiles(): Observable<File> {
+  return from(decompress(templateZip))
+    .pipe(
+      mergeAll(),
+      filter(file => file.type === 'file'),
+      map(file => new File({
+        cwd: process.cwd(),
+        base: templateZip,
+        path: path.resolve(templateZip, file.path),
+        contents: file.data
+      }))
+    );
+}
+
+function applyTemplate(templateContext: TemplateContext): MonoTypeOperatorFunction<File> {
+  return tap(file => {
+    if (file.path.endsWith('.mustache') && file.contents) {
+      const template = handlebars.compile(file.contents.toString());
+
+      file.path = file.path.substring(0, file.path.length - 9);
+      file.contents = Buffer.from(template(templateContext));
+    }
+  });
 }
